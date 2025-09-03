f05d6095c8267700e9cd87127f9eeacaa26c7fb5
drslowdeath <vikecah@gmail.com>
2025-09-03 04:07:31 +0100

    new hero bette yay

--- src/views/NewView.vue
+++ src/views/NewView.vue
@@ -1,13 +1,14 @@
 <script setup>
 import TheButton from '@/components/layout/TheButton.vue'
 import NewHero from '@/components/svg/NewHero.vue'
+import PostMench from '@/components/svg/PostMench.vue'
 </script>
 <template>
   <div class="container flex max-h-[90vh] place-content-center gap-6 place-self-center">
     <!-- text -->
     <div class="flex flex-col justify-center gap-4">
-      <div class="text-8xl font-bold tracking-tight">Hi I'm, Vik!</div>
-      <div class="font-ligh p-1 text-lg leading-tight">
+      <div class="text-8xl font-bold tracking-tight">Hi I'm Vik!</div>
+      <div class="font-ligh mt-2 p-1 text-lg">
         and I love building
 
         <div class="grad-tr-prim-sec inline-block bg-clip-text font-bold text-transparent">
@@ -27,8 +28,9 @@ import NewHero from '@/components/svg/NewHero.vue'
         <TheButton variant="secondary">Get in touch</TheButton>
       </div>
     </div>
-    <div class="w-96 max-w-full">
-      <NewHero class="text-fg-prim relative size-96"></NewHero>
+    <div class="w-full">
+      <!-- <NewHero class="text-fg-prim relative size-96"></NewHero> -->
+      <PostMench></PostMench>
     </div>
   </div>
 </template>
